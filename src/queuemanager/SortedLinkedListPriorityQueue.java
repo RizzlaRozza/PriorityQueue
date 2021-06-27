a1c258369888b25e731e537963cd272917e91d26
RizzlaRozza <55086965+RizzlaRozza@users.noreply.github.com>
2021-06-27 19:16:29 +0100

    Reworked using new encapsulated parameters following along from initial example in sortedlinklist.java removed dependancies on external packages as per assessment criteria

--- src/queuemanager/SortedLinkedListPriorityQueue.java
+++ src/queuemanager/SortedLinkedListPriorityQueue.java
@@ -1,78 +1,84 @@
 package queuemanager;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+
+/**
+ * 
+ * @author roryk
+ * @param <T> 
+ * 
+ * References:
+ *  "Add" method was developed using the following reference: https://stackoverflow.com/questions/27347940/insert-into-sorted-linked-list
+ *  "Head" method reused from SortedArrayPriorityQueue file
+ * 
  */
 
-import ch09.priorityQueues.*;
-import java.util.Comparator;
-import queuemanager.PriorityQueue;
-import queuemanager.QueueUnderflowException;
-import support.LLNode;
+
 
 public class SortedLinkedListPriorityQueue<T> implements PriorityQueue<T> {
 
-    protected LLNode<T> front;     // reference to the front of this queue
-    protected LLNode<T> rear;      // reference to the rear of this queue
+    
     protected LLNode<T> previous;   // node preceding location
     protected int numElements = 0; // number of elements in this queue
-    protected Comparator<T> storage;
     private int tailIndex;
 
+    /**
+     * variable "head" stores list item at the head of the queue
+     */    
+    private PriorityItem<T> head;
+    private PriorityItem<T> tail;
+    
+    
+    /**
+     * Sets head and tail to null
+     */
     public SortedLinkedListPriorityQueue() {
-        front = null;
-        rear = null;
+        head = null;
+        tail = null;
         tailIndex = -1;
     }
-
-    public void add(T element) {
-        // Adds element to this priority queue.
-
-        LLNode<T> newNode = new LLNode<T>(element);
-        LLNode<T> currNode = front;
-        int compare;
-        previous = null;
-
-        if (this.isEmpty()) {
-            front = newNode;
-            numElements++;
+    
+    
+    public T head() throws QueueUnderflowException{
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
         } else {
-            while (currNode != null) {
-                compare = Integer.compare((int) newNode.getInfo(), (int) currNode.getInfo());
-
-                // if they're equal attach to currNode
-                if (compare == 0) {
-                    newNode.setLink(currNode.getLink());
-                    currNode.setLink(newNode);
-                    numElements++;
-                    break;
-
-                    // if greater than, attach to previous
-                } else if (compare > 0) {
-                    newNode.setLink(currNode);
-                    if (previous == null) {
-                        front = newNode;
-                    } else {
-                        previous.setLink(newNode);
-                    }
-                    numElements++;
-                    break;
-
-                    //We reached the end of the list, add this lowest number
-                } else if (currNode.getLink() == null) {
-                    currNode.setLink(newNode);
-                    numElements++;
-                    break;
-                }
-
-                //It was not greater than currNode, loop to the next node
-                previous = currNode;
-                currNode = currNode.getLink();
-            }
+            return ((PriorityItem<T>) head).getItem();
         }
     }
 
+    /**
+     * Adds an item to the list (queue)
+     * @param item
+     * @param priority
+     * @throws QueueOverflowException 
+     */
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        
+        PriorityItem addNewItem = new PriorityItem(item, priority);
+        if (head == null){
+            head = addNewItem;
+        }
+        // insert in head if x is lower than the head
+        else if(head.getPriority() < priority){            
+            this.head = head;
+        }
+        // find the first Item in the queue which value is lower than x (or the tail)
+        else{
+             PriorityItem existing;
+            for (existing = head; existing.getPriorityItem() != null && priority >= existing.getPriorityItem().getPriority(); existing = existing.getPriorityItem());
+            // remove duplicates
+            if (priority != existing.getPriorityItem().getPriority()) {
+                addNewItem.setPriorityItem(existing.getPriorityItem());
+                existing.setPriorityItem(addNewItem);
+            }
+             
+        }
+    }
+    
+
+        
+    @Override
     public void remove() throws QueueUnderflowException {
         // Throws PriQUnderflowException if this priority queue is empty;
         // otherwise, removes element with highest priority from this 
@@ -80,23 +86,13 @@ public class SortedLinkedListPriorityQueue<T> implements PriorityQueue<T> {
         if (isEmpty()) {
             throw new QueueUnderflowException();
         } else {
-            for (int i = 0; i < tailIndex; i++) {
-                storage[i] = storage[i + 1];
+            // if there is no item after head set head to null else focus on next item in queue
+            if(head.getPriorityItem() == null) {
+                head = null;
             }
-            tailIndex--; 
-        }
-
-        if (!isEmpty()) {
-            T element;
-            element = front.getInfo();
-            front = front.getLink();
-            if (front == null) {
-                rear = null;
-            }
-            numElements--;
-            return element;
-        } else {
-            throw new QueueUnderflowException("dequeue attempted on an empty priority queue.");
+            else {
+                head = new PriorityItem<T>(head.getPriorityItem().getItem(), head.getPriorityItem().getPriority(), head.getPriorityItem().getPriorityItem());
+            }         
         }
     }
 
@@ -105,9 +101,11 @@ public class SortedLinkedListPriorityQueue<T> implements PriorityQueue<T> {
         return numElements;
     }
 
-    public boolean isEmpty() // Returns true if this priority queue is empty; otherwise, returns false.
-    {
-        return (front == null);
+    /**
+     * @return Returns true if this priority queue is empty; otherwise, returns false.
+     */
+    public boolean isEmpty() {
+        return (head == null);
     }
 
     public boolean isFull() {
@@ -130,3 +128,4 @@ public class SortedLinkedListPriorityQueue<T> implements PriorityQueue<T> {
         return result;
     }
 }
+
